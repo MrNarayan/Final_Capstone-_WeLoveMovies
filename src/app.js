136d8be9ad7bf5d3524de30c428999befb886283
nmishra <mishranarayan@hotmail.com>
2024-01-12 13:57:47 -0800

    use cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 if (process.env.USER) require("dotenv").config();
 
+const cors = require("cors");
 const express = require("express");
 const app = express();
 const notFound = require("./errors/notFound");
@@ -8,7 +9,7 @@ const theatersRouter = require("./theaters/theaters.router");
 const reviewsRouter = require("./reviews/reviews.router");
 
 // TODO: Add your code here
-
+app.use(cors());
 app.use(express.json());
 
 app.use("/movies", moviesRouter);
